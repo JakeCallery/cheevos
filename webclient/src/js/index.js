5f138dd28e06aa6aae0ad0ebc89b0c177a4b7d82
Jake Callery <jcallery@subvoicestudios.com>
2016-12-14 20:14:35 -0500

    Initial Commit basic possible folder structure

---
+++ webclient/src/js/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Jake on 12/14/2016.
+ */
---
+++ webclient/src/webpack.config.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './js/index.js'
+    ],
+
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/../dist'
+    },
+
+    resolve: {
+        modules: [
+            'node_modules',
+            './js/jacjslibes6'
+        ]
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.png$/,
+                loader: 'file-loader',
+                exclude: /node_modules/
+            }
+        ],
+
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'html/index.html'
+        })
+    ],
+
+    devtool: 'source-map'
+
+};
