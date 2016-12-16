67c554d6a14160ea5fb12baf77756940bfe9174e
Jake Callery <jcallery@subvoicestudios.com>
2016-12-15 21:55:05 -0500

    added static serving

--- webserver/app.js
+++ webserver/app.js
@@ -23,8 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'views/dist')));
+console.log(__dirname);
 app.use('/', index);
 app.use('/users', users);
 
