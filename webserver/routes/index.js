8bc3462ed6a56252a67f41b9b4756a230e7b7fc3
Jake Callery <jcallery@subvoicestudios.com>
2016-12-15 20:40:46 -0500

    testing ejs rendering with .html extension

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dist/index', { title: 'Cheevos' });
+  res.render('dist/index', { title: 'Cheevos Title' });
 });
 
 module.exports = router;
