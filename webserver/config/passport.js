bcb233e3e96ab3d7b64486a4d781805a011f40ed
Jake Callery <jcallery@subvoicestudios.com>
2016-12-29 16:10:40 -0500

    some refactoring more functions handled by User class basic serialize and deserialize user functions implemented

--- webserver/config/passport.js
+++ webserver/config/passport.js
@@ -11,49 +11,79 @@ const User = require('../models/User');
 module.exports = function(passport){
     passport.serializeUser((user, done) => {
         console.log('Serialize User Called: ', user);
-        done(null, user.id);
+        //TODO: Check for other types of user logins
+        done(null, user.data.google.id);
     });
 
     passport.deserializeUser((id, done) => {
-        // User.findById(id, function(err, user) {
-        //     done(err, user);
-        // });
         console.log('DeserializeUser Called: ', id);
-        done(null, id);
+        User.findById(id)
+            .then(($user) => {
+                if($user){
+                    console.log('deserializeUser Found User');
+                    done(null, $user);
+                } else {
+                    console.log('deserializeUser did NOT find user');
+                    done(null,false);
+                }
+            })
+            .catch((error) => {
+                console.error('Deserialize Error: ', error);
+
+            });
     });
 
     passport.use(new GoogleStrategy(
         authConfig.googleAuth,
         (accessToken, refreshToken, profile, done) => {
             process.nextTick(() => {
-                //Look up user based on profile.id
-                User.findById(profile.id)
-                    .then(($user) => {
-                      if($user){
-                          console.log('Found User: ', $user.data.name);
-                          return done(null, $user);
-                      } else {
-                          //create new user
-                          console.log('Creating New User');
-                          let newUser = new User();
-                          newUser.data.google.id = profile.id;
-                          newUser.data.google.accessToken = accessToken;
-                          newUser.data.google.refreshToken = refreshToken;
-                          newUser.data.google.name = profile.displayName;
-                          newUser.data.google.email = profile.emails[0].value;
-
-                          // newUser.saveToDB(($err) => {
-                          //       if($err){
-                          //           throw $err;
-                          //       }
-                          //       console.log('New User Created');
-                          //       return done(null, newUser);
-                          // });
-                      }
+                let idObj = {
+                    google: {
+                        id: profile.id,
+                        name: profile.displayName,
+                        email: profile.emails[0].value,
+                        accessToken: accessToken,
+                        refreshToken:  refreshToken
+                    }
+                };
+                User.findOrCreate(idObj)
+                    .then((user) => {
+                        console.log('Find or Create User: ', user);
+                        console.log('AccessToken: ', accessToken);
+                        console.log('refreshToken: ', refreshToken);
+                        console.log('profiles: ', profile);
+                        done(null, user);
                     })
                     .catch((error) => {
                         console.error('Error: ', error);
+                        done(null,false);
                     });
+                // //Look up user based on profile.id
+                // User.findById(profile.id)
+                //     .then(($user) => {
+                //       if($user){
+                //           console.log('Found User: ', $user.data.name);
+                //           return done(null, $user);
+                //       } else {
+                //           //create new user
+                //           console.log('Creating New User');
+                //           let newUser = new User();
+                //           newUser.data.google.id = profile.id;
+                //           newUser.data.google.accessToken = accessToken;
+                //           newUser.data.google.refreshToken = refreshToken;
+                //           newUser.data.google.name = profile.displayName;
+                //           newUser.data.google.email = profile.emails[0].value;
+                //
+                //           // newUser.saveToDB(($err) => {
+                //           //       if($err){
+                //           //           throw $err;
+                //           //       }
+                //           //       console.log('New User Created');
+                //           //       return done(null, newUser);
+                //           // });
+                //       }
+                //     })
+
 
                 //if error, show error
                 //if user is found, return done(null, user);
@@ -62,10 +92,7 @@ module.exports = function(passport){
                 //store the id, token, name, email
                 //save the user
                 //return done(null, newUser)
-                console.log('AccessToken: ', accessToken);
-                console.log('refreshToken: ', refreshToken);
-                console.log('profiles: ', profile);
-                done(null, profile);
+
             });
         }
     ));
--- webserver/models/User.js
+++ webserver/models/User.js
@@ -10,9 +10,66 @@ class User {
         this.data = $data || {};
     }
 
-    saveToDB($cb){
-        console.log('Saving New User To DB');
-        $cb();
+    fromGoogleIdObj($idObj){
+        console.log('FromGoogleData: ', $idObj);
+        this.data.google = {};
+        this.data.google.id = $idObj.id;
+        this.data.google.accessToken = $idObj.accessToken;
+        this.data.google.refreshToken = $idObj.refreshToken;
+        this.data.google.name = $idObj.name;
+        this.data.google.email = $idObj.email;
+    };
+
+    fromUserRecord($userRecord){
+        console.log('FromUserRecord: ', $userRecord);
+        this.data = $userRecord;
+    }
+
+    static findOrCreate($idObj){
+        if($idObj.hasOwnProperty('google')){
+            console.log('Finding By Google ID: ', $idObj.google);
+            let session = db.session();
+            return session
+                .run(
+                    'MATCH (user:User {googleID:{googleID}}) RETURN user', {googleID: $idObj.google.id}
+                )
+                .then(result => {
+                    session.close();
+                    console.log('Result: ', result);
+                    console.log('Records: ', result.records.length);
+                    if(result.records.length > 0) {
+                        console.log('Creating New User');
+                        return new Promise((resolve, reject) => {
+                            let newUser = new User();
+                            newUser.fromUserRecord(result.records[0].get('user'));
+                            resolve(newUser);
+                        })
+                    } else {
+                        console.log('No User Found');
+                        console.log('Creating a new one...');
+
+                        let newUser = new User();
+                        newUser.fromGoogleIdObj($idObj.google);
+
+                        //TODO: Async Save to DB Here
+
+                        return new Promise ((resolve, reject) => {
+                            resolve(newUser);
+                        });
+                    }
+
+                })
+                .catch((error) => {
+                    session.close();
+                    return new Promise((resolve, reject) => {
+                        reject(error);
+                    })
+                })
+        } else {
+            //Bad login ID type
+            console.error('Bad ID Type');
+            throw new Error('Bad ID Type');
+        }
     }
 
     static findById($id){
@@ -33,16 +90,16 @@ class User {
                     })
                 } else {
                     console.log('No User Found');
-                    return new Promise ((resolve, reject) => {
+                    return new Promise((resolve, reject) => {
                          resolve(null);
                     });
                 }
 
             })
             .catch((error) => {
+                console.error('Bad user Lookup: ', error);
                 session.close();
-                throw error;
-            })
+            });
 
     }
 }
