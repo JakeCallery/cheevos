4b20e10ee1942facca751757d3cd8bdfafb58999
Jake Callery <jcallery@subvoicestudios.com>
2017-03-19 13:47:25 -0400

    return 400 code if user tries to block themselves

--- webserver/routes/blockUser.js
+++ webserver/routes/blockUser.js
@@ -8,22 +8,34 @@ const User = require('../models/User');
 
 router.post('/', (req, res) => {
     console.log('Block User Request...');
-    req.cheevosData.sessionUser.blockUser(req.body.memberId)
-    .then(($dbResult) => {
-        let resObj = {
-            status:'SUCCESS'
-        };
+    let user = req.cheevosData.sessionUser;
+    if(req.body.memberId !== user.data.userId){
+        req.cheevosData.sessionUser.blockUser(req.body.memberId)
+            .then(($dbResult) => {
+                let resObj = {
+                    status:'SUCCESS'
+                };
 
-        res.status(200).json(resObj);
-    })
-    .catch(($error) => {
+                res.status(200).json(resObj);
+            })
+            .catch(($error) => {
+                console.error('blockUser Error: ', $error);
+                let resObj = {
+                    error:$error,
+                    status:'ERROR'
+                };
+                res.status(400).json(resObj);
+            });
+    } else {
+        let $error = 'Cannot block yourself';
         console.error('blockUser Error: ', $error);
         let resObj = {
             error:$error,
             status:'ERROR'
         };
         res.status(400).json(resObj);
-    });
+    }
+
 });
 
 module.exports = router;
