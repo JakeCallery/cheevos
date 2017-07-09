7836267b70900e30c2294bd61736b646bf683791
Jake Callery <jcallery@subvoicestudios.com>
2017-07-09 18:17:11 -0400

    started deployment scripts / ansible

---
+++ deployment/appnode/deployAppNode.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+ansible-playbook -i ../inventory/hosts appnode_lxc_playbook.yml
---
+++ deployment/appnode/provisionAppNode.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+ansible-playbook -i ../inventory/hosts appnode_provision_playbook.yml
