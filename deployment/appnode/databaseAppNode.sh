9ff96e98e911ee207a33825cbfe4abc06ec85c51
Jake Callery <jcallery@subvoicestudios.com>
2017-07-18 22:56:38 -0400

    working DB Deployment scripts

--- deployment/appnode/databaseAppNode.sh
+++ deployment/appnode/databaseAppNode.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ansible-playbook -v -i ../inventory/hosts appnode_database_playbook.yml
+ansible-playbook -i ../inventory/hosts appnode_database_playbook.yml
--- deployment/appnode/remoteScripts/waitForDBStart.sh
+++ deployment/appnode/remoteScripts/waitForDBStart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-end="$((SECONDS+10))"
+end="$((SECONDS+30))"
 while true; do
     [[ "200" = "$(curl --silent --write-out %{http_code} --output /dev/null http://localhost:7474)" ]] && break
     [[ "${SECONDS}" -ge "${end}" ]] && echo "DB Failed to start..." && exit 1
